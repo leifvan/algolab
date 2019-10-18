65455ae93e64edef5318330f77c37a2ee4fd351a
Leif Van Holland <holland@uni-bonn.de>
2019-10-18 13:29:45 +0200

    use sum instead of loop

--- solutions/sheet1/areapoly.py
+++ solutions/sheet1/areapoly.py
@@ -2,20 +2,16 @@ import sys
 
 # parse
 
-lines = [l.strip() for l in sys.stdin]
+lines = [l for l in sys.stdin]
 n = int(lines[0])
-points = [l.split() for l in lines[1:]]
-points = [(int(x), int(y)) for x,y in points]
+points = [tuple(int(v) for v in l.split()) for l in lines[1:]]
 
 # calc
 
 vol = 0
 i = 0
-for p1, p2 in zip(points, points[1:]+[points[0]]):
-    vol += (p1[0]+p2[0]) * (p1[1]-p2[1])
-    i += 1
-vol /= 2
 
+vol = 0.5 * sum((p1[0]+p2[0]) * (p1[1]-p2[1]) for p1, p2 in zip(points, points[1:]+[points[0]]))
 
 # output
 
