26390a8ad7486c5d346d0a6cca0547239b55af93
Leif Van Holland <holland@uni-bonn.de>
2019-10-18 12:24:39 +0200

    fix issue with generators dict not defined properly

--- generate/generators.py
+++ generate/generators.py
@@ -6,16 +6,15 @@ generators['test'] = {'test': TestGenerator}
 
 # sheet 1
 
+# TODO automate this
 from generate.sheet1.gridsnap import GridSnap
-generators['sheet1'] = {'gridsnap': GridSnap}
-
 from generate.sheet1.kmeans1 import KMeans1
-generators['sheet1'] = {'kmeans1': KMeans1}
-
 from generate.sheet1.areapoly import AreaPoly
-generators['sheet1'] = {'areapoly': AreaPoly}
-
 from generate.sheet1.matrixmul import MatrixMul
-generators['sheet1'] = {'matrixmul': MatrixMul}
+
+generators['sheet1'] = {'gridsnap': GridSnap,
+                        'kmeans1': KMeans1,
+                        'areapoly': AreaPoly,
+                        'matrixmul': MatrixMul}
 
 
