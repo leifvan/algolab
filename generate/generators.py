2a8b286d17c3f6d958265d923a4a6ff2ab51081a
Leif Van Holland <holland@uni-bonn.de>
2019-10-18 16:37:10 +0200

    automatize generator import

--- generate/generators.py
+++ generate/generators.py
@@ -1,20 +1,23 @@
+import os, inspect, importlib
+from collections import defaultdict
+
 # import all generators
-generators = dict()
+generators = defaultdict(dict)
 
-from generate.test.test import TestGenerator
-generators['test'] = {'test': TestGenerator}
-
-# sheet 1
-
-# TODO automate this
-from generate.sheet1.gridsnap import GridSnap
-from generate.sheet1.kmeans1 import KMeans1
-from generate.sheet1.areapoly import AreaPoly
-from generate.sheet1.matrixmul import MatrixMul
-
-generators['sheet1'] = {'gridsnap': GridSnap,
-                        'kmeans1': KMeans1,
-                        'areapoly': AreaPoly,
-                        'matrixmul': MatrixMul}
+def get_generator_class_from_module(mod, fname):
+    for name, data in inspect.getmembers(mod):
+        if name.lower() == fname.lower():
+            return data
+    raise AttributeError(f"Module '{mod.__name__}' does not contain a class corresponding to the file name '{fname}'.")
 
 
+base = os.path.dirname(__file__)
+
+for dirp in os.listdir(base):
+    base_dirp = os.path.join(base,dirp)
+    if os.path.isdir(base_dirp) and not dirp.startswith('__'):
+        for fp in os.listdir(base_dirp):
+            if fp.endswith('.py'):
+                name = fp[:-3]
+                mod = importlib.import_module(f"generate.{dirp}.{name}")
+                generators[dirp][name] = get_generator_class_from_module(mod, name)
