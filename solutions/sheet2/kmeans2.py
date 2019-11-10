7d01b700afbfe37f0bf3dc4de4b42793b9a2fed6
Leif Van Holland <holland@uni-bonn.de>
2019-11-10 11:17:02 +0100

    add point sorting => faster algorithm

--- solutions/sheet2/kmeans2.py
+++ solutions/sheet2/kmeans2.py
@@ -2,27 +2,37 @@ import sys
 
 # parse
 
-lines = [l.strip().replace('.','') for l in sys.stdin]
+lines = [l.strip().replace('.', '') for l in sys.stdin]
 c, q, r = [int(v) for v in lines[0].split()]
-centers = sorted(int(v) for v in lines[1:c+1])
-points = [int(v) for v in lines[c+1:]]
+centers = sorted(int(v) for v in lines[1:c + 1]) + [float('inf')]
+points = sorted(((i,int(v)) for i,v in enumerate(lines[c + 1:c+1+q])), key=lambda tup: tup[1])
 
 # calc
 
-idxs = []
-for i, p in enumerate(points):
-    j, dist = min(((j,abs(p-x)) for j,x in enumerate(centers)), key=lambda tup: tup[1])
-    idxs.append(j if dist <= r else None)
+idxs = [None] * len(points)
+ci = 0
 
+for i, p in points:
+    dist = abs(p - centers[ci])
+    dist_next = abs(p - centers[ci + 1])
+
+    while dist_next < dist:
+        ci += 1
+        dist = dist_next
+        dist_next = abs(p - centers[ci+1])
+
+    if dist <= r:
+        idxs[i] = ci
 
 # output
+
 
 def int_to_dec(i):
     sgn = '-' if i < 0 else ''
     if abs(i) < 10:
-        return sgn+"0.0"+str(abs(i))
+        return sgn + "0.0" + str(abs(i))
     elif abs(i) < 100:
-        return sgn+"0."+str(abs(i))
+        return sgn + "0." + str(abs(i))
     else:
         return str(i)[:-2] + "." + str(i)[-2:]
 
