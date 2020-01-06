3831292b1a12e02120fdb0122cc6c2369c383ba4
Leif Van Holland <holland@uni-bonn.de>
2020-01-06 13:15:59 +0100

    simplify python solution, but its still too slow

--- generate/sheet4/duckburg.py
+++ generate/sheet4/duckburg.py
@@ -1,31 +1,38 @@
 import numpy as np
 from math import floor, ceil
-
+from numba import njit
 
 class Duckburg:
     @staticmethod
     def get_random_instance():
         k = np.random.randint(1,100)
-        n = np.random.randint(1,100)
+        n = np.random.randint(1,1000)
         x = np.random.choice(np.arange(1,1001), size=n, replace=False)
         str_in = '\n'.join(str(v) for v in [k, n] + list(x))
         x = np.sort(x)
 
-        D = np.zeros((k, n))
-
-        def cc(i, j):
+        @njit
+        def cc(x, i, j):
             m_ij = (j + i) / 2
             mu_ij = (x[floor(m_ij)] + x[ceil(m_ij)]) / 2
-            return sum(abs(x[l] - mu_ij) for l in range(i, j + 1))
+            return np.abs(x[i:j+1]-mu_ij).sum()
 
-        for m in range(n):
-            D[0, m] = cc(0, m)
+        @njit
+        def determine_result(k,n,x):
+            D = np.zeros((k, n))
 
-        for i in range(1, k):
-            for m in range(1, n):
-                D[i, m] = min(D[i - 1, j - 1] + cc(j, m) for j in range(m + 1))
+            for m in range(n):
+                D[0, m] = cc(x, 0, m)
 
-        str_out = str(int(25 * n - D[-1, -1]))
+            for i in range(1, k):
+                for m in range(1, n):
+                    D[i, m] = min([D[i - 1, j - 1] + cc(x, j, m) for j in range(m + 1)])
+
+            return D[-1,-1]
+
+        result = determine_result(k,n,x)
+
+        str_out = str(int(25 * n - result))
         return str_in, str_out
 
 
--- solutions/sheet4/duckburg.py
+++ solutions/sheet4/duckburg.py
@@ -1,23 +1,29 @@
 import sys
 from math import floor, ceil
+from itertools import accumulate
+from operator import add
 
 k = int(sys.stdin.readline())
 n = int(sys.stdin.readline())
+
+if k >= n:
+    print(25*n)
+    exit(0)
+
 x = sorted(int(line) for line in sys.stdin)
+prefix_sums = [0] + list(accumulate(x, add))
 
 
 def cc(i, j):
     m_ij = (j + i) / 2
+    fm_ij = floor(m_ij)
     mu_ij = (x[floor(m_ij)] + x[ceil(m_ij)]) / 2
-    return sum(abs(x[l] - mu_ij) for l in range(i, j + 1))
+    return (2 * fm_ij - i - j + 1) * mu_ij - 2 * prefix_sums[fm_ij + 1] + prefix_sums[i] + prefix_sums[j + 1]
 
 
-lastD = [cc(0,m) for m in range(n)]
-curD = [0] * n
+D = [cc(0, m) for m in range(n)]
 
 for i in range(1, k):
-    for m in range(1, n):
-        curD[m] = min(lastD[j - 1] + cc(j, m) for j in range(m + 1))
-    lastD = list(curD)
+    D = [0] + [min(D[j - 1] + cc(j, m) for j in range(1, m + 1)) for m in range(1, n)]
 
-print(int(25 * n - lastD[-1]))
+print(25 * n - int(D[-1]))
